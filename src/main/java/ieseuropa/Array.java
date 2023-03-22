e4f643bfc11f600e46a42d64c987659e5fbdd03a
nika215 <monica.antona@educa.madrid.org>
2023-03-22 20:34:26 +0100

    metodo media

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,17 @@
 package main.java.ieseuropa;
 
 public class Array {
+	private static int media(int[]array) {
+		int suma = 0;
+		for(int i=0;i<array.length;i++) {
+				suma += array[i];
+		}
+		return suma / array.length;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		int[] array = {2,5,56,17,45,23,9,122,1};
+		System.out.println("La media es " + media(array));
 	}
 
 }
