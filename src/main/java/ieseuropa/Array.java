bc85a2ea6c8abc52f17c50730470b8a11f541077
Mel-Gr <melanina.greek@gmail.com>
2023-03-23 17:43:59 +0100

    Añadido metodo mediana. Esperando validacion.

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,21 @@
 package main.java.ieseuropa;
 
 public class Array {
+	
+	public static int maximo(int[]a) {
+		int max = 0;
+		for (int i = 0; i < a.length; i++) {
+			if(a[i]>max)
+				max = a[i];
+		}
+		return max;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		int[]a = {3, 4, 2, 6, 7};
+		System.out.println(maximo(a));
 	}
 
 }
