19164f05792aa24a84d1f191fe8e5e9814f690db
PatriDominguez <122823076+PatriDominguez@users.noreply.github.com>
2023-03-24 16:45:25 +0100

    Numero de la suerte completado

--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -1,6 +1,29 @@
 package main.java.ieseuropa;
 
+import java.util.ArrayList;
+
 public class Numero {
+
+	private static String numeroSuerte(int numero) {
+		ArrayList<Integer> numeros = new ArrayList<>();
+		for (int i = 1; i <= numero; i++) {
+			numeros.add(i);
+		}
+		numeros.removeIf(num -> (num % 2 == 0));
+		int pos = 1;
+		while (pos < numeros.size()) {
+			int lista = numeros.get(pos);
+			for (int i = lista - 1; i < numeros.size(); i += lista - 1) {
+				numeros.remove(i);
+			}
+			pos++;
+		}
+		if (numeros.contains(numero)) {
+			return "es un numero de la suerte";
+		} else {
+			return "no es un numero de la suerte";
+		}
+	}
 
 	private static int calcularFactorial(int numero) {
 		int factorial = 1;
@@ -13,7 +36,7 @@ public class Numero {
 	public static void main(String[] args) {
 		int numero = 5;
 		System.out.println("Factorial: " + numero + " != " + calcularFactorial(numero));
+		System.out.println("El numero " + numero + " " + numeroSuerte(numero));
 
 	}
-
 }
