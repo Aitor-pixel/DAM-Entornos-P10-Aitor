a61e49f6fd8a97cdf5aea1afb73a218cebf52117
PatriDominguez <122823076+PatriDominguez@users.noreply.github.com>
2023-03-24 16:39:52 +0100

    Calcular mediana completado

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,6 +1,18 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
+
 public class Array {
+	
+	private static float calcularMediana(int [] a) {
+		int mitad = a.length/2;
+		Arrays.sort(a);
+		if (a.length % 2 == 0) {
+			return (a[mitad - 1] + a[mitad]) / 2;
+		} else {
+			return a[mitad];
+		}
+	}
 	
 	public static int maximo(int[]a) {
 		int max = 0;
@@ -16,6 +28,7 @@ public class Array {
 		System.out.println("Hola");
 		int[]a = {3, 4, 2, 6, 7};
 		System.out.println(maximo(a));
+		System.out.println("La mediana es: " + calcularMediana(a));
 	}
 
 }
