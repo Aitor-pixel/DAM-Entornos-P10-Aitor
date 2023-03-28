d3d3e45717466c98b4d1235b7cc391b3c2f921c1
monica <monica.antona@educa.madrid.org>
2023-03-28 12:40:53 +0200

    Agregar calcular longitud nombre

---
+++ src/main/java/ieseuropa/CadenaDeTexto.java
@@ -0,0 +1,11 @@
+package main.java.ieseuropa;
+
+public class CadenaDeTexto {
+	private static int calcularLongitudNombre(String cadena) {
+		return cadena.length();
+	}
+	public static void main(String[] args) {
+		System.out.println(calcularLongitudNombre("github"));
+	}
+
+}
