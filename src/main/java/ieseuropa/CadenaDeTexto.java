84e27ef329c68997b44583347a348654526d841b
monica <monica.antona@educa.madrid.org>
2023-03-28 13:30:12 +0200

    Agregar repetirOcurrencias

--- src/main/java/ieseuropa/CadenaDeTexto.java
+++ src/main/java/ieseuropa/CadenaDeTexto.java
@@ -1,11 +1,21 @@
 package main.java.ieseuropa;
 
+
 public class CadenaDeTexto {
 	private static int calcularLongitudNombre(String cadena) {
 		return cadena.length();
 	}
+	private static String repetirOcurrencias(String texto) {
+		String repetido = "";
+		for (int i = 0; i < texto.length(); i++) {
+			repetido += texto.charAt(i);
+			repetido += texto.charAt(i);
+		}
+		return repetido;
+	}
 	public static void main(String[] args) {
 		System.out.println(calcularLongitudNombre("github"));
+		System.out.println(repetirOcurrencias("NBA"));
 	}
 
 }
